a69db0889239c4a70bd8fd9e3b8af2017e81c3c7
Jmok19927 <jmok19927@gmail.com>
2022-06-28 12:06:24 -0700

    took out client test

--- database/index.js
+++ database/index.js
@@ -1,12 +1,6 @@
 const mongoose = require('mongoose');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://<username>:<p4ssword>@cluster0.ji0dqfy.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
 mongoose.connect(uri);
 
