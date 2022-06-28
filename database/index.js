7d448ebd1ee3210c2afc4163e4bdab8968fcb229
Jmok19927 <jmok19927@gmail.com>
2022-06-28 12:03:35 -0700

    hooking up mongoose to mongo atlas

--- database/index.js
+++ database/index.js
@@ -1,5 +1,16 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://<username>:<p4ssword>@cluster0.ji0dqfy.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+mongoose.connect(uri);
+
+
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
